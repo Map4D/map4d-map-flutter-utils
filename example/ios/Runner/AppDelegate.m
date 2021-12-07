d20557b53e8bbde6f46c49921759fef817e4ea65
Huy Dang <caohuy@live.com>
2021-12-07 15:34:43 +0700

    Update gitignore & load map example

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,12 +1,14 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import <Map4dMap/MFServices.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
+  NSAssert(false, @"Provide a valid key registered with the demo app bundle id. Then delete this line.");
+  [MFServices provideAccessKey:@""];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
